acea2ea0a969ae9203d68f8b9152f9df04283ac3
Shubham Sinha <shubham.sinha2512@gmail.com>
2022-05-04 17:18:08 +0530

    hardcoded env variables for deployment

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
-const DB_USER = process.env.DB_USER;
-const DB_PWD = process.env.DB_PWD;
-const DB_NAME = process.env.DB_NAME;
+const PORT = process.env.PORT || 5000;
+const DB_USER = process.env.DB_USER || 'shubham';
+const DB_PWD = process.env.DB_PWD || 'shubham';
+const DB_NAME = process.env.DB_NAME || 'omdb-app';
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PWD}@testcluster.4cqpg.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
     {
--- utils/JWTUtils.js
+++ utils/JWTUtils.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken')
 
+const JWT_EXPIRES = '30d';
+const JWT_SECRET = 'myjwtsecret123';
+
 exports.signToken = id => {
-    return jwt.sign({id: id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES})
+    return jwt.sign({id: id}, process.env.JWT_SECRET || JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES || JWT_EXPIRES})
 }
 
 exports.verifyToken = token => {
-    return jwt.verify(token, process.env.JWT_SECRET)
+    return jwt.verify(token, process.env.JWT_SECRET || JWT_SECRET)
 }
