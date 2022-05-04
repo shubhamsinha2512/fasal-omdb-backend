abab20f13f58ab7919d90df494985823d3d6588a
Shubham Sinha <shubham.sinha2512@gmail.com>
2022-05-04 17:13:05 +0530

    create empty list enabled

--- controllers/MovieListController.js
+++ controllers/MovieListController.js
@@ -5,78 +5,81 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
 
 
-exports.createMovieList = catchAsync(async (req, res, next)=>{
+exports.createMovieList = catchAsync(async (req, res, next) => {
 
-    if(req.body){
+    if (req.body) {
+        let moviesAddedId = [];
 
         let listCreateObj = {
-            user : req.user,
-            listName : req.body.listName
+            user: req.user,
+            listName: req.body.listName
         }
 
         let newList = await MovieList.create(listCreateObj);
-       
-        if(!Array.isArray(req.body.movies)){
-            return next(new AppError("movies should be an array"), 400)
+
+        if (req.body.movies !== undefined) {
+
+            if (!Array.isArray(req.body.movies)) {
+                return next(new AppError("movies should be an array"), 400)
+            }
+
+            let moviesUserAdded = req.body.movies.map(movie => {
+                return {
+                    ...movie,
+                    user: req.user
+                }
+            })
+
+            let moviesAdded = await Movie.insertMany(moviesUserAdded);
+            moviesAddedId = moviesAdded.map(m => m._id)
         }
 
-        let moviesUserAdded = req.body.movies.map(movie => {
-            return {
-                ...movie,
-                user: req.user
-            }
-        }) 
+        let newListUpdated = await MovieList.findByIdAndUpdate(newList._id, { $push: { 'movies': { $each: moviesAddedId } } }, { new: true });
 
-        let moviesAdded = await Movie.insertMany(moviesUserAdded);
-        let moviesAddedId = moviesAdded.map(m => m._id)
-        let newListUpdated = await MovieList.findByIdAndUpdate(newList._id, {$push: {'movies' : {$each: moviesAddedId}}}, {new:true});
-        
         res.status(200).json({
-            status:'success',
-            data:{
+            status: 'success',
+            data: {
                 movieList: newListUpdated
             }
         })
 
-    }else{
+    } else {
         next(new AppError('Please provide request body', 400))
     }
 })
 
 
-exports.getMovieLists = catchAsync(async (req, res, next)=>{
+exports.getMovieLists = catchAsync(async (req, res, next) => {
 
-    const movieLists = await MovieList.find({user : req.user});
-    console.log(movieLists)
+    const movieLists = await MovieList.find({ user: req.user });
 
-    if(Array.isArray(movieLists)){
+    if (Array.isArray(movieLists)) {
 
         res.status(200).json({
-            status:'success',
-            data:{
+            status: 'success',
+            data: {
                 movieLists: movieLists
             }
         })
 
-    }else{
+    } else {
         next(new AppError('Cannot retrive movie lists'), 500)
     }
 })
 
 
-exports.updateMovieList = catchAsync(async (req, res, next)=>{
+exports.updateMovieList = catchAsync(async (req, res, next) => {
 
-    if(req.body){
+    if (req.body) {
 
         let list = await MovieList.findById(req.params.listId);
-        let moviesAddedId = []
-       
-        if(req.body.movies !== undefined){
+        let moviesAddedId = [];
 
-            if(!Array.isArray(req.body.movies)){
+        if (req.body.movies !== undefined) {
+            if (!Array.isArray(req.body.movies)) {
                 return next(new AppError("movies should be an array"), 400)
             }
-            
+
             let moviesUserAdded = req.body.movies.map(movie => {
                 return {
                     ...movie,
@@ -88,17 +91,17 @@ exports.updateMovieList = catchAsync(async (req, res, next)=>{
             moviesAddedId = moviesAdded.map(m => m._id)
         }
 
-        let {movies, ...otherFields} = req.body;
-        let newListUpdated = await MovieList.findByIdAndUpdate(list._id, {...otherFields, $push: {'movies' : {$each: moviesAddedId}}}, {new:true});
+        let { movies, ...otherFields } = req.body;
+        let newListUpdated = await MovieList.findByIdAndUpdate(list._id, { ...otherFields, $push: { 'movies': { $each: moviesAddedId } } }, { new: true });
 
         res.status(200).json({
-            status:'success',
-            data:{
+            status: 'success',
+            data: {
                 movieList: newListUpdated
             }
         })
 
-    }else{
+    } else {
         next(new AppError('Please provide request body', 400))
     }
 })
